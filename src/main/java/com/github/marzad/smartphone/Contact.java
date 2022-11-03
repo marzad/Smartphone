3262d842ea4a591ea34c02867e314de1ac7fc4b8
marzad <maschatz@gmx.net>
2022-11-03 16:00:22 +0100

    Add Contact Update gitignore

---
+++ src/main/java/com/github/marzad/smartphone/Contact.java
@@ -0,0 +1,9 @@
+package com.github.marzad.smartphone;
+
+public abstract class Contact {
+    String name;
+
+    public String getName(){
+        return this.name;
+    }
+}
--- src/main/java/com/github/marzad/smartphone/Main.java
+++ src/main/java/com/github/marzad/smartphone/Main.java
@@ -1,4 +1,6 @@
-package com.github.marzad.smartphone;public class Main {
+package com.github.marzad.smartphone;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
